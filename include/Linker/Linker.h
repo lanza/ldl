8934f0de53d98b328f66857c6d675069e53a0180
Nathan Lanza <nathan@lanza.io>
2018-02-18 19:56:51 -0800

    4.3 - Handle general segment names

--- include/Linker/Linker.h
+++ include/Linker/Linker.h
@@ -33,7 +33,10 @@ using ObjectFilePtr = std::unique_ptr<ObjectFile>;
 
     std::vector<Symbol> MergedSymbols;
     std::vector<Relocation> MergedRelocation;
-    std::vector<Segment> MergedSegments;
+    using Permissions = std::string;
+    using SegmentName = std::string;
+    std::map<Permissions, std::map<SegmentName, std::vector<Segment>>> SegmentDataStructure;
+
 
     Linker(std::vector<std::string> FileNames) :FileNames{FileNames} { }
     Linker() { }
@@ -51,33 +54,45 @@ using ObjectFilePtr = std::unique_ptr<ObjectFile>;
     }
 
     ObjectFilePtr GenerateObjectFile() {
-      GenerateOutputFileSegments();
       GenerateOutputFileSymbolTable();
+      GenerateOutputFileSegments();
       ObjectFilePtr OFPtr = ObjectFilePtr{new ObjectFile{}};
-      OFPtr->FH = FileHeader{"LINK", 3, 0, 0};
-      OFPtr->Segments = {TextSegment, DataSegment, BSSSegment};
+      OFPtr->FH = FileHeader{
+        "LINK",
+        static_cast<int>(RPSegments.size() + RWPSegments.size() + RWSegments.size()),
+        static_cast<int>(MergedSymbols.size()),
+        static_cast<int>(MergedRelocation.size())
+      };
+      std::vector<Segment> Ss;
+      Ss.insert(Ss.end(), RPSegments.begin(), RPSegments.end());
+      Ss.insert(Ss.end(), RWPSegments.begin(), RWPSegments.end());
+      Ss.insert(Ss.end(), RWSegments.begin(), RWSegments.end());
+      OFPtr->Segments = std::move(Ss);
 
       return OFPtr;
     }
 
-    void MergeSegmentsIntoOneVector() {
+    void MergeSegmentsIntoDataStructure() {
       for (auto& OFPtr : ObjectFiles) {
-        MergedSegments.insert(MergedSegments.end(), OFPtr->Segments.begin(), OFPtr->Segments.end());
+        for (auto& S : OFPtr->Segments) {
+          SegmentDataStructure[S.Code][S.Name].push_back(S);
+        }
       }
     }
 
-    OutSegment TextSegment{"a.out", ".text", 0x1000, 0x0, "RP"};
-    OutSegment DataSegment{"a.out", ".data", 0x0, 0x0, "RWP"};
-    OutSegment BSSSegment{"a.out", ".bss", 0x0, 0x0, "RW"};
-    Segment CommonSegment{"NA", ".common", 0x0, 0x0, "RWP"};
-    std::vector<OutSegment> OtherSegments;
+    OutSegment* TextSegment;
+    std::vector<OutSegment> RPSegments;
+    OutSegment* DataSegment;
+    std::vector<OutSegment> RWPSegments;
+    OutSegment* BSSSegment;
+    Segment CommonSegment{"NA", ".common", 0x0, 0x0, "RW"};
+    std::vector<OutSegment> RWSegments;
 
 
     void GenerateOutputFileSymbolTable() {
       MergeSymbolsIntoOneVector();
       GatherCommonSymbols();
       CombineCommonSymbolsIntoCommonSegment();
-      AppendBSSSegment(CommonSegment);
     }
 
     void CombineCommonSymbolsIntoCommonSegment() {
@@ -102,46 +117,96 @@ using ObjectFilePtr = std::unique_ptr<ObjectFile>;
     }
 
     void GenerateOutputFileSegments() {
-      MergeSegmentsIntoOneVector();
-      InitializeTextSegment();
-      MergeTextSegments();
-      FinalizeTextSegment();
-      InitializeDataSegment();
-      MergeDataSegments();
-      FinalizeDataSegment();
-      InitializeBSSSegment();
-      MergeBSSSegments();
-      FinalizeBSSSegment();
+      MergeSegmentsIntoDataStructure();
+
+      MergeRPSegments();
+      MergeRWPSegments();
+      MergeRWSegments();
     }
 
-    void MergeTextSegments() {
-      for (auto& S : MergedSegments) {
-        if (S.Name == ".text")
-          AppendTextSegment(S);
-      }
-    }
-    void MergeDataSegments() {
-      for (auto& S : MergedSegments) {
-        if (S.Name == ".data")
-          AppendDataSegment(S);
-    }
-    }
-    void MergeBSSSegments() {
-      for (auto& S : MergedSegments) {
-        if (S.Name == ".bss")
-          AppendBSSSegment(S);
+    void MergeRPSegments() {
+      auto& RPSegmentsDataStructure = SegmentDataStructure["RP"];
+      auto TextSegments = RPSegmentsDataStructure[".text"];
+      RPSegmentsDataStructure.erase(".text");
+      RPSegments.push_back(OutSegment{"a.out", ".text", 0x1000, 0x0, "RP"});
+      TextSegment = &RPSegments.front();
+
+      for (auto& S : TextSegments)
+        AppendTextSegment(S);
+
+
+      for (auto& NameVectorPair : RPSegmentsDataStructure) {
+        OutSegment RPSegment{"a.out", NameVectorPair.first, 0x0, 0x0, "RP"};
+        InitializeSegmentForSamePage(RPSegment, RPSegments.back());
+        for (auto& S : NameVectorPair.second)
+          AppendSegmentToOutSegment(S, RPSegment);
+        RPSegments.push_back(RPSegment);
       }
     }
 
+    void MergeRWPSegments() {
+      auto& RWPSegmentsDataStructure = SegmentDataStructure["RWP"];
+      auto DataSegments = RWPSegmentsDataStructure[".data"];
+      RWPSegmentsDataStructure.erase(".data");
+      RWPSegments.push_back(OutSegment{"a.out", ".data", 0x0, 0x0, "RWP"});
+      DataSegment = &RWPSegments.front();
+
+      InitializeSegmentForNewPage(*DataSegment, RPSegments.back());
+      for (auto& S : DataSegments)
+        AppendDataSegment(S);
+
+      for (auto& NameVectorPair : RWPSegmentsDataStructure) {
+        OutSegment RWPSegment{"a.out", NameVectorPair.first, 0x0, 0x0, "RWP"};
+        InitializeSegmentForSamePage(RWPSegment, RWPSegments.back());
+        for (auto& S : NameVectorPair.second)
+          AppendSegmentToOutSegment(S, RWPSegment);
+        RWPSegments.push_back(RWPSegment);
+      }
+    }
+
+    void MergeRWSegments() {
+      auto& RWSegmentsDataStructure = SegmentDataStructure["RW"];
+      auto BSSSegments = RWSegmentsDataStructure[".bss"];
+      RWSegmentsDataStructure.erase(".bss");
+      RWSegments.push_back(OutSegment{"a.out", ".bss", 0x0, 0x0, "RW"});
+      BSSSegment = &RWSegments.front();
+
+      InitializeSegmentForSamePage(*BSSSegment, RWPSegments.back());
+      for (auto& S : BSSSegments)
+        AppendBSSSegment(S);
+
+      AppendBSSSegment(CommonSegment);
+
+      for (auto& NameVectorPair : RWSegmentsDataStructure) {
+        OutSegment RWSegment{"a.out", NameVectorPair.first, 0x0, 0x0, "RW"};
+        InitializeSegmentForSamePage(RWSegment, RWSegments.back());
+        for (auto& S : NameVectorPair.second)
+          AppendSegmentToOutSegment(S, RWSegment);
+        RWSegments.push_back(RWSegment);
+      }
+    }
+
+    void InitializeSegmentForSamePage(OutSegment& Next, OutSegment& Previous) {
+      int ModuloFour = Previous.Length % 4;
+      int BlankSpaceSize = (4 - ModuloFour) % 4;
+
+      Previous.Length += BlankSpaceSize;
+      for (int i = 0; i < BlankSpaceSize; i++)
+        Previous.Data += "00";
+      Next.Address = Previous.Address + Previous.Length;
+    }
+    void InitializeSegmentForNewPage(OutSegment& Next, OutSegment& Previous) {
+      Next.Address = ((Previous.Address + Previous.Length) & 0xF000) + 0x1000;
+    }
 
     void AppendTextSegment(Segment S) {
-      AppendSegmentToOutSegment(S, TextSegment);
+      AppendSegmentToOutSegment(S, *TextSegment);
     }
     void AppendDataSegment(Segment S) {
-      AppendSegmentToOutSegment(S, DataSegment);
+      AppendSegmentToOutSegment(S, *DataSegment);
     }
     void AppendBSSSegment(Segment S) {
-      AppendSegmentToOutSegment(S, BSSSegment);
+      AppendSegmentToOutSegment(S, *BSSSegment);
     }
 
 
@@ -161,26 +226,6 @@ using ObjectFilePtr = std::unique_ptr<ObjectFile>;
 
       Mappings[S.FileName][S.Name] = SegmentMapping(S.Name, ContainedSegment.Address, ContainedSegment.Address + ContainedSegment.Length -1);
     }
-
-    void InitializeTextSegment() {
-    }
-    void FinalizeTextSegment() {}
-    void InitializeDataSegment() {
-
-      DataSegment.Address = ((TextSegment.Address + TextSegment.Length) & 0xF000) + 0x1000;
-    }
-    void FinalizeDataSegment() {
-      int ModuloFour = DataSegment.Length % 4;
-      int BlankSpaceSize = (4 - ModuloFour) % 4;
-
-      DataSegment.Length += BlankSpaceSize;
-      for (int i = 0; i < BlankSpaceSize; i++)
-        DataSegment.Data += "00";
-    }
-    void InitializeBSSSegment() {
-      BSSSegment.Address = DataSegment.Address + DataSegment.Length;
-    }
-    void FinalizeBSSSegment() {}
   };
 }
 
--- tests/LinkerTests/LinkerTest.cpp
+++ tests/LinkerTests/LinkerTest.cpp
@@ -42,13 +42,10 @@ TEST_F(LinkerTest, CreatsCommonSegment) {
 }
 
 TEST_F(LinkerTest, AppendsCommonSegmentOntoBSS) {
+  L.GenerateOutputFileSymbolTable();
   L.GenerateOutputFileSegments();
-  L.MergeSymbolsIntoOneVector();
-  L.GatherCommonSymbols();
-  L.CombineCommonSymbolsIntoCommonSegment();
-  L.AppendBSSSegment(L.CommonSegment);
 
-  ldl::OutSegment B = L.BSSSegment;
+  ldl::OutSegment& B = *L.BSSSegment;
 
   EXPECT_THAT(B.Address, Eq(0x2000 + 0x29 + 0x3 + 0x29 + 0x3));
   EXPECT_THAT(B.Length, Eq(0x34 + 0x34 + 0x8));
@@ -65,103 +62,44 @@ TEST_F(LinkerTest, ReadsFiles) {
   EXPECT_THAT(L.ObjectFiles.size(), Eq(2));
 }
 
-TEST_F(LinkerTest, IsInitializedProperly) {
-  L.InitializeTextSegment();
-  EXPECT_THAT(L.TextSegment.Length, Eq(0));
-}
-
-TEST_F(LinkerTest, MergesSegmentsIntoOneVector) {
-  L.MergeSegmentsIntoOneVector();
-  EXPECT_THAT(L.MergedSegments.size(), Eq(6));
-}
-
-TEST_F(LinkerTest, AppendsOneTextSegment) {
-  L.MergeSegmentsIntoOneVector();
-  L.InitializeTextSegment();
-  auto& S = L.MergedSegments[0];
-  EXPECT_THAT(S.Name, Eq(".text"));
-
-  L.AppendTextSegment(S);
-
-  ldl::OutSegment& T = L.TextSegment;
-
-  EXPECT_THAT(T.Address, Eq(0x1000));
-  EXPECT_THAT(T.Length, Eq(0x3b));
-
-  ldl::Segment C = T.ContainedSegments[0];
-
-  EXPECT_THAT(C.Address, Eq(0x1000));
-  EXPECT_THAT(C.Length, Eq(0x3b));
-}
 
 TEST_F(LinkerTest, MergesTextSegments) {
-  L.MergeSegmentsIntoOneVector();
-  L.InitializeTextSegment();
-  L.MergeTextSegments();
-  L.FinalizeTextSegment();
+  L.MergeSegmentsIntoDataStructure();
+  L.MergeRPSegments();
 
-  ldl::OutSegment T = L.TextSegment;
+  ldl::OutSegment& T = *L.TextSegment;
 
   EXPECT_THAT(T.Address, Eq(0x1000));
   EXPECT_THAT(T.Length, Eq(0x3b + 0x1 + 0x3b));
   EXPECT_THAT(T.ContainedSegments.size(), Eq(2));
   EXPECT_THAT(T.InputSegments.size(), Eq(2));
 
+  ldl::Segment C = T.ContainedSegments[0];
+  EXPECT_THAT(C.Address, Eq(0x1000));
+  EXPECT_THAT(C.Length, Eq(0x3b));
+
   ldl::Segment C2 = T.ContainedSegments[1];
 
   EXPECT_THAT(C2.Address, Eq(0x1000 + 0x3b + 0x1));
   EXPECT_THAT(C2.Length, Eq(0x3b));
 }
-TEST_F(LinkerTest, InitializesDataSegmentProperly) {
-  L.MergeSegmentsIntoOneVector();
-  L.InitializeTextSegment();
-  L.MergeTextSegments();
-  L.FinalizeTextSegment();
-  L.InitializeDataSegment();
-  ldl::OutSegment& D = L.DataSegment;
-
-  EXPECT_THAT(D.Address, Eq(0x2000));
-  EXPECT_THAT(D.Length, Eq(0x0));
-}
-
-
-TEST_F(LinkerTest, AppendsADataSegmentProperly) {
-  L.MergeSegmentsIntoOneVector();
-  L.InitializeTextSegment();
-  L.MergeTextSegments();
-  L.FinalizeTextSegment();
-  L.InitializeDataSegment();
-
-  auto& S = L.MergedSegments[1];
-  EXPECT_THAT(S.Name, Eq(".data"));
-
-  L.AppendDataSegment(S);
-
-  ldl::OutSegment& D = L.DataSegment;
-
-  EXPECT_THAT(D.Address, Eq(0x2000));
-  EXPECT_THAT(D.Length, Eq(0x29));
-
-  ldl::Segment C = D.ContainedSegments[0];
-
-  EXPECT_THAT(C.Address, Eq(0x2000));
-  EXPECT_THAT(C.Length, Eq(0x29));
-}
 
 TEST_F(LinkerTest, MergesDataSegments) {
-  L.MergeSegmentsIntoOneVector();
-  L.InitializeTextSegment();
-  L.MergeTextSegments();
-  L.FinalizeTextSegment();
-  L.InitializeDataSegment();
-  L.MergeDataSegments();
+  L.MergeSegmentsIntoDataStructure();
+  L.MergeRPSegments();
+  L.MergeRWPSegments();
 
-  ldl::OutSegment D = L.DataSegment;
+  ldl::OutSegment& D = *L.DataSegment;
 
   EXPECT_THAT(D.Address, Eq(0x2000));
   EXPECT_THAT(D.Length, Eq(0x29 + 0x3 + 0x29));
   EXPECT_THAT(D.ContainedSegments.size(), Eq(2));
   EXPECT_THAT(D.InputSegments.size(), Eq(2));
+
+  ldl::Segment C = D.ContainedSegments[0];
+
+  EXPECT_THAT(C.Address, Eq(0x2000));
+  EXPECT_THAT(C.Length, Eq(0x29));
 
   ldl::Segment C2 = D.ContainedSegments[1];
 
@@ -170,81 +108,45 @@ TEST_F(LinkerTest, MergesDataSegments) {
 }
 
 TEST_F(LinkerTest, FinalizesDataSegmentProperly) {
-  L.MergeSegmentsIntoOneVector();
-  L.InitializeTextSegment();
-  L.MergeTextSegments();
-  L.FinalizeTextSegment();
-  L.InitializeDataSegment();
-  L.MergeDataSegments();
-  L.FinalizeDataSegment();
+  L.MergeSegmentsIntoDataStructure();
+  L.MergeRPSegments();
+  L.MergeRWPSegments();
 
-  ldl::OutSegment D = L.DataSegment;
+  ldl::OutSegment& D = *L.DataSegment;
 
-  EXPECT_THAT(D.Length, Eq(0x29 + 0x3 + 0x29 + 0x3));
+  EXPECT_THAT(D.Length, Eq(0x29 + 0x3 + 0x29));
 }
 
 TEST_F(LinkerTest, InitializesBSSandFinalizesDataProperly) {
-  L.MergeSegmentsIntoOneVector();
-  L.InitializeTextSegment();
-  L.MergeTextSegments();
-  L.FinalizeTextSegment();
-  L.InitializeDataSegment();
-  L.MergeDataSegments();
-  L.FinalizeDataSegment();
-  L.InitializeBSSSegment();
+  L.MergeSegmentsIntoDataStructure();
+  L.MergeRPSegments();
+  L.MergeRWPSegments();
+  L.MergeRWSegments();
 
-  ldl::OutSegment D = L.DataSegment;
-  ldl::OutSegment B = L.BSSSegment;
+  ldl::OutSegment& D = *L.DataSegment;
+  ldl::OutSegment& B = *L.BSSSegment;
 
   EXPECT_THAT(D.Length, Eq(0x29 + 0x3 + 0x29 + 0x3));
-  EXPECT_THAT(B.Length, Eq(0x0));
   EXPECT_THAT(B.Address, Eq(0x2000 + 0x29 + 0x3 + 0x29 + 0x3));
 }
 
-TEST_F(LinkerTest, AppendsABSSSegmentProperly) {
-  L.MergeSegmentsIntoOneVector();
-  L.InitializeTextSegment();
-  L.MergeTextSegments();
-  L.FinalizeTextSegment();
-  L.InitializeDataSegment();
-  L.MergeDataSegments();
-  L.FinalizeDataSegment();
-  L.InitializeBSSSegment();
+TEST_F(LinkerTest, MergesBSSSegments) {
+  L.MergeSegmentsIntoDataStructure();
+  L.MergeRPSegments();
+  L.MergeRWPSegments();
+  L.MergeRWSegments();
 
-  ldl::Segment S = L.MergedSegments[2];
-
-  EXPECT_THAT(S.Name, Eq(".bss"));
-
-  L.AppendBSSSegment(S);
-
-  ldl::OutSegment& B = L.BSSSegment;
+  ldl::OutSegment& B = *L.BSSSegment;
 
   EXPECT_THAT(B.Address, Eq(0x2000 + 0x29 + 0x3 + 0x29 + 0x3));
-  EXPECT_THAT(B.Length, Eq(0x34));
+  EXPECT_THAT(B.Length, Eq(0x34 + 0x34));
+  EXPECT_THAT(B.ContainedSegments.size(), Eq(3));
+  EXPECT_THAT(B.InputSegments.size(), Eq(3));
 
   ldl::Segment C = B.ContainedSegments[0];
 
   EXPECT_THAT(C.Address, Eq(0x2000 + 0x29 + 0x3 + 0x29 + 0x3));
   EXPECT_THAT(C.Length, Eq(0x34));
-}
-
-TEST_F(LinkerTest, MergesBSSSegments) {
-  L.MergeSegmentsIntoOneVector();
-  L.InitializeTextSegment();
-  L.MergeTextSegments();
-  L.FinalizeTextSegment();
-  L.InitializeDataSegment();
-  L.MergeDataSegments();
-  L.FinalizeDataSegment();
-  L.InitializeBSSSegment();
-  L.MergeBSSSegments();
-
-  ldl::OutSegment B = L.BSSSegment;
-
-  EXPECT_THAT(B.Address, Eq(0x2000 + 0x29 + 0x3 + 0x29 + 0x3));
-  EXPECT_THAT(B.Length, Eq(0x34 + 0x34));
-  EXPECT_THAT(B.ContainedSegments.size(), Eq(2));
-  EXPECT_THAT(B.InputSegments.size(), Eq(2));
 
   ldl::Segment C2 = B.ContainedSegments[1];
 
@@ -255,7 +157,7 @@ TEST_F(LinkerTest, MergesBSSSegments) {
 TEST_F(LinkerTest, MergesAllSegments) {
   L.GenerateOutputFileSegments();
 
-  ldl::OutSegment T = L.TextSegment;
+  ldl::OutSegment& T = *L.TextSegment;
 
   EXPECT_THAT(T.Address, Eq(0x1000));
   EXPECT_THAT(T.Length, Eq(0x3b + 0x1 + 0x3b));
@@ -267,7 +169,7 @@ TEST_F(LinkerTest, MergesAllSegments) {
   EXPECT_THAT(TC2.Address, Eq(0x1000 + 0x3b + 0x1));
   EXPECT_THAT(TC2.Length, Eq(0x3b));
 
-  ldl::OutSegment D = L.DataSegment;
+  ldl::OutSegment& D = *L.DataSegment;
 
   EXPECT_THAT(D.Address, Eq(0x2000));
   EXPECT_THAT(D.Length, Eq(0x29 + 0x3 + 0x29 + 0x3));
@@ -279,12 +181,12 @@ TEST_F(LinkerTest, MergesAllSegments) {
   EXPECT_THAT(DC2.Address, Eq(0x2000 + 0x29 + 0x3));
   EXPECT_THAT(DC2.Length, Eq(0x29));
 
-  ldl::OutSegment B = L.BSSSegment;
+  ldl::OutSegment& B = *L.BSSSegment;
 
   EXPECT_THAT(B.Address, Eq(0x2000 + 0x29 + 0x3 + 0x29 + 0x3));
   EXPECT_THAT(B.Length, Eq(0x34 + 0x34));
-  EXPECT_THAT(B.ContainedSegments.size(), Eq(2));
-  EXPECT_THAT(B.InputSegments.size(), Eq(2));
+  EXPECT_THAT(B.ContainedSegments.size(), Eq(3));
+  EXPECT_THAT(B.InputSegments.size(), Eq(3));
 
   ldl::Segment BC2 = B.ContainedSegments[1];
 
